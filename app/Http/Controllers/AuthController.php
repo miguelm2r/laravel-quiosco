bdb4cebf2e5ae16f46b20ce95d671264cac2660e
miguelm2r <2miguelmr@gmail.com>
2023-07-06 12:23:07 -0600

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegistroRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request){
+        $data = $request->validated();
+
+        // Revisar el password
+
+        if(!Auth::attempt($data)){
+            return response([
+                'errors' => ['El email o el password son incorrectos']
+            ], 422);
+        }
+
+        // Autenticar al usuario
+
+        $user = Auth::user();
+
+        return [
+            'user' => $user,
+            'token' => $user->createToken('token')->plainTextToken,
+        ];
+    }
+
+    public function register(RegistroRequest $request){
+        // Validar el registro
+        $data = $request->validated();
+
+        // Crear el usuario
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        // Retornar respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
+    }
+
+    public function logout(Request $request){
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return [
+            'user' => null
+        ];
+    }
+
+
+}
